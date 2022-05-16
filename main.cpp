8df1f5e2fec71388322a6a4d18a9e6ea3563d236
Frans <97081479+Fransssss@users.noreply.github.com>
2022-05-16 02:01:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+// Title: C-String Functions
+// Purpose: Create a program that does several C-string functions (strlen, strchr, strcmp)
 // Author : Fransiskus Agapa
+// Have fun - Enjoy the process - Practices make improvement
 
 #include "Menu.h"
 
